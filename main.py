f5d44aef601da3463f9b720d7bc17bd480806ec9
mona-1029 <mona-kei-haikyu@outlook.jp>
2026-06-04 21:35:56 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import discord
 from discord.ext import commands
 
-TOKEN = "ここにBotトークン"
+TOKEN = os.environ["DISCORD_TOKEN"]
 
 intents = discord.Intents.default()
 intents.message_content = True
