dbb11b1bbba4294adfc09058fa5001f17fef020a
mona-1029 <mona-kei-haikyu@outlook.jp>
2026-06-04 21:09:44 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import discord
+from discord.ext import commands
 
+TOKEN = "ここにBotトークン"
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} でログインしました")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content == "ポモドーロ":
+        await message.channel.send("🍅 ポモドーロBot準備中")
+
+    await bot.process_commands(message)
+
+bot.run(TOKEN)
